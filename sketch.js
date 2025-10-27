0aade7f972f11df076ba89fd840732bfb4b6355b
Mr-Polack <126186511+Mr-Polack@users.noreply.github.com>
2025-10-27 14:56:37 -0400

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,24 @@
+let ball;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  ball = new Sprite();
+  ball.x = width / 2 - 200;
+  ball.diameter = 50;
+
+  for (let i = 0; i < 10; i++) {
+    let block = new Sprite(
+      width / 2 + random(-5, 5),
+      height / 2 + random(-5, 5),
+      40,
+      40
+    );
+  }
+}
+
+function draw() {
+  background(220);
+  ball.moveTowards(mouse, 0.1);
+}
+
+function mousePressed() {}
